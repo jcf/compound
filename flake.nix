56eda0eb3e99d263fce29adf30a1e68ec174a9de
James Conroy-Finn <git@invetica.co.uk>
2023-08-28 18:03:01 +0100

    Manage dev environment with Nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Compound";
+
+  inputs = {
+    devenv.url = "github:cachix/devenv";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, devenv }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            ({ pkgs, ... }: {
+              packages = with pkgs; [ clj-kondo clojure clojure-lsp git ];
+            })
+          ];
+        };
+      });
+}
