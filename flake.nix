3c4628d272c4968ed8934b54ce28c58c7d7890ff
James Conroy-Finn <git@invetica.co.uk>
2023-08-28 18:14:34 +0100

    Manage frontend and processes with devenv

--- flake.nix
+++ flake.nix
@@ -9,13 +9,41 @@
 
   outputs = { self, flake-utils, nixpkgs, devenv }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        overlays = [
+          (self: super: rec {
+            nodejs = super.nodejs-18_x;
+            pnpm = super.nodePackages.pnpm;
+            yarn = (super.yarn.override { inherit nodejs; });
+          })
+        ];
+        pkgs = import nixpkgs { inherit overlays system; };
       in {
         devShells.default = devenv.lib.mkShell {
           inherit inputs pkgs;
           modules = [
             ({ pkgs, ... }: {
-              packages = with pkgs; [ clj-kondo clojure clojure-lsp git ];
+              env.OVERMIND_PORT = "3000";
+
+              packages = with pkgs; [
+                babashka
+                bash
+                clj-kondo
+                clojure
+                clojure-lsp
+                git
+                htmlq
+                node2nix
+                nodejs
+                pnpm
+              ];
+
+              processes = {
+                nrepl.exec = "clojure -M:dev:test -m compound.dev";
+                tailwind.exec = "pnpm watch";
+              };
+
+              process.implementation = "overmind";
             })
           ];
         };
